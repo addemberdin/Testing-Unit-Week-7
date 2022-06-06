a467793d84bb685178a7e51b9e9580ee8cde9742
Adde Berdin <addemberdin@gmail.com>
2022-06-06 07:34:36 -0400

    Create .js

---
+++ .js
@@ -0,0 +1,17 @@
+function greeting(name){
+  if (name === null) {
+    return 'Hey there!';
+  }
+  if (typeof name === typeof []) {
+    if (name.length > 0) {
+      let greetingMultiple = '';
+      greetingMultiple.forEach((elm) => {
+        greetingMultiple += ', ' + elm; 
+   )};
+    return 'Hey' + greetingMultiple + '!';
+  }
+  return "Hey there!';
+}
+if (name === name.toUppersCare()) {
+  return 'HELLO, ' + name + '!';
+}
